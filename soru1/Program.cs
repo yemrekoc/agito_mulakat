e5dc4769a70e9ca3cd6c2b2e492c914242b3a948
yunus emre koç <kocyemre@hotmail.com>
2021-06-16 12:04:55 +0300

    Add project files.

---
+++ soru1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soru1
+{
+    class Program
+    {
+        public static Random rnd = new Random(); 
+        
+        static void Main(string[] args)
+        {
+            int[] liste = new int[9];
+            int sayac = 0;
+            for (int i = 0; i < liste.Length; i++)
+            {
+            deger:
+                var value = rnd.Next(1, 10);
+                sayac++;
+                if (i == 0)
+                {
+                    DegerAta(liste, i, value);
+
+                }
+                else if (DegerBul(liste, i, value))
+                {
+                    DegerAta(liste, i, value);
+                }
+                else
+                {
+                    goto deger;
+                }
+
+
+            }
+
+            for (int i = 0; i < liste.Length; i++)
+            {
+                Console.Write(liste[i].ToString());
+                if (i != liste.Length - 1)
+                {
+                    Console.Write(",");
+                }
+
+            }
+            Console.Write("  Random fonksiyonu {0} kere çalışmıştır.", sayac);
+            Console.Read();
+
+        }
+
+    public static bool DegerBul(int[] liste, int index, int deger)
+    {
+        for (int i = 0; i < index; i++)
+        {
+            if (liste[i] == deger)
+            {
+                return false;
+            }
+
+        }
+        if (liste[index - 1] == deger)
+        {
+            return false;
+        }
+
+
+        return true;
+
+    }
+    public static void DegerAta(int[] liste, int index, int deger)
+    {
+        liste[index] = deger;
+    }
+}
+}
---
+++ soru2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input:");
+           
+            string input = Console.ReadLine();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == 0)
+                {
+                    dict.Add(input[i], 0);
+                }
+                else if (input[i] == input[i-1])
+                {
+                    dict.Remove(input[i]);
+                    dict.Add(input[i], 2);
+                }
+                else
+                {
+                    if (!dict.ContainsKey(input[i]))
+                    {
+                        dict.Add(input[i], 1);
+                    }
+                }
+            }
+
+            Console.WriteLine("Output:");
+            foreach (var item in dict)
+            {
+                for (int i = 0; i < item.Value; i++)
+                {
+                    Console.Write("-");
+                }
+                Console.WriteLine(item.Key);
+            }
+
+            Console.Read();
+        }
+    }
+
+}
